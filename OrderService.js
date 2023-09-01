d18c06264a72d50454cc9c5d5da80bff56ced632
ThamaraiP <tamibe@gmail.com>
2023-09-01 19:18:23 +0530

    Update OrderService.js

--- OrderService.js
+++ OrderService.js
@@ -2,36 +2,27 @@ exports.handler =  async (event, context) => {
   
   
   var AWS = require("aws-sdk");
-  var sns = new AWS.SNS();
-  let responseCode = 200;
+  
+  let message =  "Order Placed Successfully";
   console.info('Publish to SNS', event);
-  await sns.publish({
-      TopicArn: "arn:aws:sns:ap-south-1:982353931640:Order",
-      Message: JSON.stringify(event)
-  }, function(err, data) {
-      if(err) {
-          console.error('error publishing to SNS');
-          
-      } else {
-          console.info('message published to SNS');
-          
-      }
-  }).promise();
+  
+  
+  const sns = new AWS.SNS({ apiVersion: '2010-03-31' });
+  await sns
+    .publish(
+      {
+        TopicArn: "arn:aws:sns:ap-south-1:982353931640:Order",
+        Message: JSON.stringify(event)
+      },
+    ).promise();
+  
   
   let responseBody = {
-        message: "Order Placed Successfully",
+        message: message,
         orderId: event.orderId
     };
 
-  
-  let response = {
-        statusCode: responseCode,
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(responseBody)
-    };
-    console.log("response: " + JSON.stringify(response))
+    
     return responseBody;
 
 
